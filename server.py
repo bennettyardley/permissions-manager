068663d096d58f75ce430052ec814eada088631a
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-06-18 21:29:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sqlite3, threading, time, os
 
 conn = sqlite3.connect('requests.db')
-d = conn.cursor()
+c = conn.cursor()
 
 def removePermission(values):
     #setfacl -m u:USERNAME:PERMS -R DIRECTORY
